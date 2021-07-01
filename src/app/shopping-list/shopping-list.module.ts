4b2e395c2cd631b4209c97051f976a9b42365d72
Your Name <you@example.com>
2021-07-01 17:49:20 +0530

    moved the shopping list to separate component

---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
+import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
+import { ShoppingListComponent } from "./shopping-list.component";
+
+const appRoutes: Routes = [
+    { path: 'shopping-list', component: ShoppingListComponent },
+  ];
+
+@NgModule({
+    declarations:[
+        ShoppingListComponent,
+        ShoppingEditComponent
+    ],
+    imports:[CommonModule,FormsModule,RouterModule.forChild(appRoutes)]
+})
+
+export class ShoppingListModule{}
