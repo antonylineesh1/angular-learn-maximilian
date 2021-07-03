1c0e7cc94a586e30d44b51a1a60955d4bac5cc54
Your Name <you@example.com>
2021-07-03 12:36:31 +0530

    multiple actions

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -3,9 +3,17 @@ import { Action } from "@ngrx/store";
 import {Ingredient } from '../../shared/ingredient.model';
 
 export const ADD_INGREDIENT='ADD_INGREDIENT';
+export const ADD_INGREDIENTS='ADD_INGREDIENTS';
 	
 export class AddIngredient implements Action
 {
     type: string=ADD_INGREDIENT;
-    payload:Ingredient;
+    constructor(public payload:Ingredient){}
 }
+
+export class AddIngredients implements Action{
+    type:string=ADD_INGREDIENTS;
+    constructor(public payload:Ingredient[]){}
+}
+
+export type ShoppingListActions= AddIngredient | AddIngredients;
--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,19 +1,24 @@
 import { Action } from "@ngrx/store";
 import { Ingredient } from "../../shared/ingredient.model";
-import * as ShoppinglistActions from './shopping-list.actions';
+import * as shoppinglistActions from './shopping-list.actions';
 
 const initalState = {
   ingredients: [new Ingredient("Apples", 5), new Ingredient("Tomatoes", 10)],
 };
 
-export function ShoppingListReducer(state = initalState, action:ShoppinglistActions.AddIngredient) {
+export function ShoppingListReducer(state = initalState, action:any) {
 
     switch (action.type) {
-        case ShoppinglistActions.ADD_INGREDIENT:
+        case shoppinglistActions.ADD_INGREDIENT:
             return {
                 ...state,
                 ingredients:[ ...state.ingredients,action.payload ]
-             }            
+             }           
+        case shoppinglistActions.ADD_INGREDIENTS:
+            return {
+                ...state,
+                ingredients:[ ...state.ingredients,...action.payload]
+                }           
          default:
             return state;
     }
